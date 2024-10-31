854885c9cdbba3ebc2c2d41cfa871492c22ace08
Lucas Eduardo <lucased18@gmail.com>
2024-10-31 16:14:22 -0300

    style
    add alguns estilos nos cards renderizadas por api

--- project-root/main.js
+++ project-root/main.js
@@ -1,38 +1,41 @@
 import './styles/global.css';
+import './styles/news-card.css';
 import $ from "jquery";
 
 function getNews() {
-  $.ajax({
-      url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=2dc0825f6234474ab137f53b8add4125',
-      method: 'GET',
-      success: function(response) {
-          console.log(response.articles);
-          const output = $('#output');
-          try {
-              response.articles.forEach(article => {
-                  output.append(`
-                      <div class="card">
-                          <div class="card-body">
-                              <img src="${article.urlToImage}" class="card-img-top" alt="${article.title}" title="${article.title}"><br>
-                              <h2 class="card-title">${article.title}</h2>
-                              <div class="card-text">
-                                  <p>${article.description}</p>
-                              </div>
-                          </div>
-                      </div>
-                      <br>
-                  `);
-                  console.log(article.title);
-              });
-          } catch (err) {
-              console.log(err);
-          }
-          $('#copyright').html(response.copyright);
-      },
-      error: function(err) {
-          console.log(err);
-      }
-  });
+    $.ajax({
+        url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=2dc0825f6234474ab137f53b8add4125',
+        method: 'GET',
+        success: function(response) {
+            console.log(response.articles);
+            const output = $('#output');
+            try {
+                response.articles.forEach((article, index) => {
+                    if (article.title !== '[Removed]' && article.description !== null) { 
+                        output.append(`
+                            <div class="card news-card" id="news-card-${index}">
+                                <div class="card-body">
+                                    <img src="${article.urlToImage}" class="card-img-top news-img" alt="${article.title}" title="${article.title}"><br>
+                                    <div class="card-text news-text">
+                                        <h2 class="card-title news-title">${article.title}</h2>
+                                        <p>${article.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                        `);
+                        console.log(article.title);
+                    }
+                });
+            } catch (err) {
+                console.log(err);
+            }
+            $('#copyright').html(response.copyright);
+        },
+        error: function(err) {
+            console.log(err);
+        }
+    });
 }
 
 getNews();
