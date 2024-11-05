99d3c2b65e25af60657b8d5ac5ea810e268b2124
Lucas Eduardo <lucased18@gmail.com>
2024-11-04 22:02:03 -0300

    feat
    add newsAside.js

--- project-root/js/newsAside.js
+++ project-root/js/newsAside.js
@@ -1,18 +1,17 @@
-import './styles/global.css';
-import './styles/news-card.css';
 import $ from "jquery";
+import '../styles/news-aside.css';
 
-const API_URL = 'https://newsapi.org/v2/top-headlines';
+const API_URL_ASIDE = 'https://newsapi.org/v2/top-headlines';
 const API_KEY = '2dc0825f6234474ab137f53b8add4125';
 
-function appendArticle(article, index) {
+function appendAside(article, index) {
     if (article.title !== '[Removed]' && article.description !== null) {
-        $('#output').append(`
-            <div class="card news-card" id="news-card-${index}">
-                <div class="card-body">
+        $('.card-aside').append(`
+            <div class="card news-card-aside" id="news-card-aside-${index}">
+                <div class="card-body-aside">
                     <img src="${article.urlToImage}" class="card-img-top news-img" alt="${article.title}" title="${article.title}"><br>
                     <div class="card-text news-text">
-                        <h2 class="card-title news-title">${article.title}</h2>
+                        <h2 class="card-title news-title-aside">${article.title}</h2>
                         <p>${article.description}</p>
                     </div>
                 </div>
@@ -23,12 +22,12 @@ function appendArticle(article, index) {
 
 function getNews() {
     $.ajax({
-        url: `${API_URL}?country=us&category=business&apiKey=${API_KEY}`,
+        url: `${API_URL_ASIDE}?country=us&category=sports&apiKey=${API_KEY}`,
         method: 'GET',
         success: function(response) {
             try {
                 response.articles.forEach((article, index) => {
-                    appendArticle(article, index);
+                    appendAside(article, index);
                     console.log(article.title);
                 });
             } catch (err) {
