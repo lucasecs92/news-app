a37892aa2863b3ac317503f4f8d1face2a23949a
Lucas Eduardo <lucased18@gmail.com>
2024-11-12 15:36:28 -0300

    update
    style up

--- project-root/js/navbarBottom.js
+++ project-root/js/navbarBottom.js
@@ -11,37 +11,36 @@ function loadNavbarBottom() {
   let isMenuOpen = false;
 
   const navbarHTML = `
-        <section id="nav-bottom-wrap">
-            <menu id="menu-icons">
-                ${
-                  isMenuOpen
-                    ? '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L17.94 6M18 18L6.06 6"/></svg>'
-                    : '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>'
-                }
-            </menu>
-            <ul id="ul-list">
-                <li>
-                    <a href="entretenimento" id="entertainment">Entretenimento</a>
-                </li>
-                <li>
-                    <a href="negocios" id="business">Negócios</a>
-                </li>
-                <li>
-                    <a href="esportes" id="sports">Esportes</a>
-                </li>
-                <li>
-                    <a href="saude" id="health">Saúde</a>
-                </li>
-                <li>
-                    <a href="tecnologia" id="technology">Tecnologia</a>
-                </li>
-                <li>
-                    <a href="ciencia" id="science">Ciência</a>
-                </li>
-            </ul>
-        </section>
-    `;
-
+    <section id="nav-bottom-wrap">
+      <menu id="menu-icons">
+        ${
+          isMenuOpen
+            ? '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L17.94 6M18 18L6.06 6"/></svg>'
+            : '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>'
+        }
+      </menu>
+      <ul id="ul-list">
+        <li>
+          <a href="entretenimento" id="entertainment">Entretenimento</a>
+        </li>
+        <li>
+          <a href="negocios" id="business">Negócios</a>
+        </li>
+        <li>
+          <a href="esportes" id="sports">Esportes</a>
+        </li>
+        <li>
+          <a href="saude" id="health">Saúde</a>
+        </li>
+        <li>
+          <a href="tecnologia" id="technology">Tecnologia</a>
+        </li>
+        <li>
+          <a href="ciencia" id="science">Ciência</a>
+        </li>
+      </ul>
+    </section>
+  `;
   $("#navbar-bottom").html(navbarHTML);
 
   const toggleMenu = () => {
--- project-root/js/navbarMain.js
+++ project-root/js/navbarMain.js
@@ -3,56 +3,17 @@ import "../styles/navbarMain.css";
 
 function loadNavbarMain() {
   const navbarHTML = `
-        <section id="navbar-wrap">
-            <section id="nav-date-wrap">
-                <span id="nav-date"></span>
-            </section>
-
-            <section id="nav-logo">
-                <a href="/">
-                  <h1>News App</h1>
-                </a>
-            </section>
-
-            <section id="nav-search">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-                    <path fill="currentColor" fill-rule="evenodd" d="M18.319 14.433A8.001 8.001 0 0 0 6.343 3.868a8 8 0 0 0 10.564 11.976l.043.045l4.242 4.243a1 1 0 1 0 1.415-1.415l-4.243-4.242zm-2.076-9.15a6 6 0 1 1-8.485 8.485a6 6 0 0 1 8.485-8.485"/>
-                </svg>
-                <input 
-                    ref="searchField"
-                    id="search-field" 
-                    type="text" 
-                    placeholder="BUSCAR"
-                >
-            </section>
-        </section>
-    `;
-
+    <section id="navbar-main-wrap">
+      <section id="nav-logo">
+        <a href="/">
+          <h1>News App</h1>
+        </a>
+      </section>
+    </section>
+  `;
   $("#navbar-main").html(navbarHTML);
 }
 
-// API de data, World Time API
-function loadDate() {
-  $.ajax({
-    url: "https://worldtimeapi.org/api/timezone/America/Sao_Paulo",
-    method: "GET",
-    success: function (response) {
-      const currentDateTime = new Date(response.datetime);
-      const formattedDate = currentDateTime.toLocaleString("pt-BR", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      });
-      $("#nav-date").html(`${formattedDate}`);
-    },
-    error: function (err) {
-      console.error("Erro ao obter a data: ", err);
-    },
-  });
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-  loadNavbarMain();
-  loadDate();
+    loadNavbarMain();
 });
