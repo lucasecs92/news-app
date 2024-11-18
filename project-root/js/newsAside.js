f0aaf73c81e930a341ab6c4b12244d0129b1f765
Lucas Eduardo <lucased18@gmail.com>
2024-11-18 20:51:33 -0300

    up

--- project-root/js/newsAside.js
+++ project-root/js/newsAside.js
@@ -6,18 +6,46 @@ const API_KEY = "2dc0825f6234474ab137f53b8add4125";
 
 // Variável de ambiente para controlar o modo de desenvolvimento
 // const isDevelopment = true; // Ajuste para true quando estiver desenvolvendo
+function timeSince(date) {
+  const seconds = Math.floor((new Date() - new Date(date)) / 1000);
+  let interval = Math.floor(seconds / 31536000);
+  if (interval > 1) {
+    return `Há ${interval} anos`;
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return `Há ${interval} meses`;
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return `Há ${interval} dias`;
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return `Há ${interval} horas`;
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return `Há ${interval} minutos`;
+  }
+  return `Há ${Math.floor(seconds)} segundos`;
+}
 
 function appendAside(article, index) {
   if (article.title !== "[Removed]" && article.description !== null) {
+    const timeElapsed = timeSince(article.publishedAt);
+
     $("#aside-news").append(`
-      <section class="card news-card-aside" id="news-card-aside-${index}">
+      <section class="news-card-aside" id="news-card-aside-${index}">
         <section class="card-body-aside">
-          <img src="${article.urlToImage}" class="card-img-top news-img-aside" alt="${article.title}" title="${article.title}">
+          <img src="${article.urlToImage}" class="news-img-aside" alt="${article.title}" title="${article.title}">
           
           <section class="card-text news-text-aside">
             <h2 class="news-title-aside">${article.title}</h2>
-            <p>${article.description}</p>
+            <p class="news-description">${article.description}</p>
+            <p class="news-time-published">${timeElapsed}</p>
           </section>
+          
         </section>
       </section>
     `);
@@ -67,9 +95,9 @@ function getNews() {
 
 getNews();
 
-// Executa getNews ou getMockNews com base no modo de desenvolvimento 
-// if (isDevelopment) { 
-//     getMockNews(); 
-// } else { 
-//     getNews(); 
+// Executa getNews ou getMockNews com base no modo de desenvolvimento
+// if (isDevelopment) {
+//     getMockNews();
+// } else {
+//     getNews();
 // }
