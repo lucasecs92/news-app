4fed3d57c963cc663a44c93207547b4271c97f39
Lucas Eduardo <lucased18@gmail.com>
2024-11-21 16:20:22 -0300

    up

--- project-root/js/sports.js
+++ project-root/js/sports.js
@@ -3,6 +3,31 @@ import $ from "jquery";
 const API_URL = 'https://newsapi.org/v2/top-headlines';
 const API_KEY = '2dc0825f6234474ab137f53b8add4125';
 
+function timeSince(date) {
+  const seconds = Math.floor((new Date() - new Date(date)) / 1000);
+  let interval = Math.floor(seconds / 31536000);
+  if (interval > 1) {
+    return `Há ${interval} anos`;
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return `Há ${interval} meses`;
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return `Há ${interval} dias`;
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return `Há ${interval} horas`;
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return `Há ${interval} minutos`;
+  }
+  return `Há ${Math.floor(seconds)} segundos`;
+}
+
 export function loadSportsContent() {
   $.ajax({
     url: `${API_URL}?country=us&category=sports&apiKey=${API_KEY}`,
@@ -11,6 +36,8 @@ export function loadSportsContent() {
       try {
         const sportsHTML = response.articles.map((article, index) => {
           if (article.title !== '[Removed]' && article.description !== null) {
+            const timeElapsed = timeSince(article.publishedAt);
+
             return `
               <section class="card news-card" id="news-card-${index}">
                 <section class="card-body">
@@ -18,6 +45,7 @@ export function loadSportsContent() {
                   <section class="card-text news-text">
                     <h2 class="card-title news-title">${article.title}</h2>
                     <p>${article.description}</p>
+                    <p class="news-time-published">${timeElapsed}</p>
                   </section>
                 </section>
               </section>
