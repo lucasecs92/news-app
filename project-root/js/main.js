141815f35135e0473a2c6a6d077b11e97b2c6868
Lucas Eduardo <lucased18@gmail.com>
2024-11-26 15:13:06 -0300

    style
    responsividade em andamento

--- project-root/js/main.js
+++ project-root/js/main.js
@@ -9,7 +9,7 @@ function appendArticle(article, index) {
     $("#main-news").append(`
       <section class="main-news-card" id="main-news-card-${index}">
         <section class="card-body">
-          <img src="${article.urlToImage}" class="main-news-img" alt="${article.title}" title="${article.title}"><br>
+          <img src="${article.urlToImage}" class="main-news-img" alt="${article.title}" title="${article.title}">
           <section class="main-news-text">
             <h2 class="main-news-title">${article.title}</h2>
             <p class="main-news-description">${article.description}</p>
@@ -21,27 +21,61 @@ function appendArticle(article, index) {
   }
 }
 
+// function getNews() {
+//   $.ajax({
+//     url: `${API_URL}?country=${COUNTRY}&category=${CATEGORY_GENERAL}&apiKey=${API_KEY}`,
+//     method: "GET",
+//     success: function (response) {
+//       if (response.articles) {
+//         response.articles.forEach((article, index) => {
+//           appendArticle(article, index);
+//           console.log(article.title);
+//         });
+//       } else {
+//         displayError("Nenhuma notícia encontrada.");
+//       }
+//     },
+//     error: function (err) {
+//       console.error("Erro ao buscar notícias: ", err);
+//       displayError(
+//         "Erro ao buscar notícias. Por favor, tente novamente mais tarde."
+//       );
+//     },
+//   });
+// }
+
 function getNews() {
-  $.ajax({
-    url: `${API_URL}?country=${COUNTRY}&category=${CATEGORY_GENERAL}&apiKey=${API_KEY}`,
-    method: "GET",
-    success: function (response) {
-      if (response.articles) {
-        response.articles.forEach((article, index) => {
-          appendArticle(article, index);
-          console.log(article.title);
-        });
-      } else {
-        displayError("Nenhuma notícia encontrada.");
-      }
-    },
-    error: function (err) {
-      console.error("Erro ao buscar notícias: ", err);
-      displayError(
-        "Erro ao buscar notícias. Por favor, tente novamente mais tarde."
-      );
-    },
-  });
+  // Verifica se os dados já estão armazenados no sessionStorage
+  if (sessionStorage.getItem("newsData")) {
+    const newsData = JSON.parse(sessionStorage.getItem("newsData"));
+    newsData.forEach((article, index) => {
+      appendArticle(article, index);
+    });
+  } else {
+    $.ajax({
+      url: `${API_URL}?country=${COUNTRY}&category=${CATEGORY_GENERAL}&apiKey=${API_KEY}`,
+      method: "GET",
+      success: function (response) {
+        try {
+          // Armazena os dados no sessionStorage
+          sessionStorage.setItem("newsData", JSON.stringify(response.articles));
+          response.articles.forEach((article, index) => {
+            appendArticle(article, index);
+            console.log(article.title);
+          });
+        } catch (err) {
+          displayError("Erro ao processar artigos.");
+          console.error("Erro ao processar artigos: ", err);
+        }
+      },
+      error: function (err) {
+        console.error("Erro ao buscar notícias: ", err);
+        displayError(
+          "Erro ao buscar notícias. Por favor, tente novamente mais tarde."
+        );
+      },
+    });
+  }
 }
 
 getNews();
